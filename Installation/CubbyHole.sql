006b71bc0f12d3f679a74e5eb291edc0f5f499f0
Alexis Chevalier <alexis.chevalier1@gmail.com>
2014-04-02 23:46:44 +0200

    Updated SQL Script for DevCenter

--- Installation/CubbyHole.sql
+++ Installation/CubbyHole.sql
@@ -30,20 +30,6 @@ CREATE TABLE `AccessTokens` (
   KEY `clientID` (`clientID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=116 ;
 
---
--- Dumping data for table `AccessTokens`
---
-
-INSERT INTO `AccessTokens` (`id`, `userID`, `clientID`, `token`) VALUES
-(42, 1, 7, 'VwndUUBwhw6zsx7SDsFP9ED9441SSegSf6aBxUO2Qjld2c2dly86K4LG6dENo4lxlPDoZRhX8EgyXvsHcG1La9coiVvRTQWM1sKRX3YRGaquSyVTlWAcZMZV482PZMg8TZNjOQsUq6trYFonUx7f55l5fSK9eDOXYJRYdx2NWWvk3Ys7zNk9WaoE5MozKmBQ6LyvD94AdLSf6Yk1AXOMBXd4IG1GSMzDl4Eb4Y80bm4pzXEKq1qVmQCPYHdNOQik'),
-(62, 45, 7, 'mJNDXZ24YKLt36m8jgNFK89KcVlJ2VDKoEEHsqlPsR6xhdjIQXRy3lTosIzaTGTTptouTxUToC6ms0ugwzzdvEPZmX6cOgZjN2g4RT7l06qfE0yRBrkxqhFlhrRgqkLUXjkx8WYZOW742n1rjor844BPcGbKtTO9opVM3A15iSq0kSe2ZHVAQr7KkkoOGLYf9sQD3yCrjChzNJwsO7DFlEzLmL1voJwLCw36KSdF2d3gXLcYyp2gvh3MFE2rQVP0'),
-(65, 46, 7, 'qriDxCL81jDxPOK5zwKN4km7q9v8nVQ0IMA0p1JNpeQ1Eh9rHqzujKcGrOwGNEaUsVnIcmeiKlYp6iUl2lmoYZNzhTILXkSsWEEfjjLFr7dln65LTxgbjtrbfhWsegja3ITNcC5LBKSUzSv2cpru1apKEIvei9Uf17oMCVebJwc64yPwpd9u0rQfAZXwxhZn5sCZCAyQh40u8ucrNST3tMlyUf2ROCoszMXtGFW0atVYDEoAQ2IqAZwsbFUWp70l'),
-(70, 48, 7, 'TmpRWSKqPF6bJKh4nqp54KrJ6zWli1lVcoySmmrvAFatSOzkl0yqoInSWDhYqtDSUP5nH1krVJRsgsrhpAx2Rs5Tqs5PHSS1OJj7G9lcVNw9VJlLAn9y08whFJPPWf7K1MXRN7nR9mKxqkG2MXyi3OyKxy2aZTlFeRZITMVRsvVjmT6Vfuevb8RFxbL3VelPzAiFDuXmhmKVQY6hlsMdUJMMawTGjq7wbQUWNYFMzeMJ64v3rlVwWiAV6xyzVkZ0'),
-(99, 51, 7, 'lhmesJDmsqrOCkjSDvkqJXpNKRQ63aBjP4X77jxIrbDgoKaEbMGJcrxGvZ0RZ7wXwlY6iEH8BkLcSyGhrV6VVDJxLcfTeA7QuWZuQBNcOfP7cj4x29eggxKYHA1FV4dpdY3aVIxavyUZDaFau5SICjtJz7Ar1fvFkvknI1izv6ftWLPG9hWTjdd57S7SgOb4TTyD7RcvMAjzFBXmbClYzIGbRPaeLsxrrVuyn1cfxMLUvTacQGTLllk9i488U0rE'),
-(108, 52, 7, 'Uo6QOBwYue4fKEfHDZqHnBU3Cd2d97J5Npse4Yul13jubGVcSNVaHPXpuUHb63WlEqbdAPeYyOBGpCcK9B5OoRUXiTJcvDU60Osxr5LZclcJUXIFj1125yTaE5mI2rOjhGM0ajKqU32B9f3lzeU25OLexQ9Hl5aBv0Q8x1D3ISwDEeFMfOb8jf3RcUInrOMUaCjJBdhrwBvStJe8qjivwYuhdkiZ9flrxFa5IK8an7blXNyMM3rHQKoUjBBF34NL'),
-(109, 53, 7, '4gu8ypWsV4FqFstpaJIuq2KECK9IExuOkePb4jfX27FYQ0A2WMP8jhWuPeUkmzbtwgyxpgzBEdSDtonoOEgqCfonQpJvgcYzKwWhyylSAJ2i6E47k6xue6n5RiLzZ13w1QhWrkgqepKW6KE3Ft4fIJr9nQWLuKSRX5GqH4rXFhGuyoxHsuD7jyW5KFaf77OctZKt6PEEOPAg0Qh8WY1u3fk6kWTcNwUSSCg1YQcgTQ9MvY0RCXHrvKncFHQRnqti'),
-(110, 54, 7, 'lsQE5msLplp6yLKuP8hMF06DY5ypNc35xTXJlJqRKQ95x7OBovZ9vOd21ZdQiKE623zHgzTsKzgS1gnj92hccrqhL8Ni7oCks5djl2jI6jiXQwxyTJSi087B5IVXjfHvakXHWZFmLrGgBjQorQggSiO1EDWbjNqvlltqo7XZOMAnus66deBvPW6ssouMQkk0yxaoM3Jc6XFONIuU7Wze1JmF7FuvgziEjLKV4cfd2LpDVsc4lkzGy2eNq8ifrZbI');
-
 -- --------------------------------------------------------
 
 --
@@ -85,12 +71,8 @@ CREATE TABLE `Clients` (
 --
 
 INSERT INTO `Clients` (`id`, `name`, `clientId`, `clientSecret`, `redirect_uri`, `dialog_disabled`, `userID`) VALUES
-(1, 'Heavenstar', '8af2ab46-ca25-4339-a93e-a709cd8825c9', 'bb5547f8-5e77-4bf6-b496-570e3b50ea20', 'https://localhost:8444', 0, 0),
-(2, 'CubbyHole Website', 'cubbyh_bbda45a6-99d3-4313-bc86-7121d3ea52d3', '23491abc-89fb-4b00-a6b1-2093fd0b0021', 'https://localhost:8443', 0, 0),
-(3, 'Test', 'test_35701cd7-6cdc-4f14-b907-41f28b621f95', '5607d031-f37a-489f-b0f9-2f4a584c3816', 'https://localhost:8443', 0, 1),
-(5, 'Test2', 'test2_52519769-0793-4b47-9844-c934bd0cf7e6', '53d08c55-648a-4026-b656-f6db8a4e82e5', 'https://localhost:8443', 0, 1),
-(7, 'CubbyHole_Website', 'cubbyh_b1175f3f-9c52-4205-adfd-c9a63f7cbecb', 'df8b08e6-215f-49ac-86fa-1212e7c9e0e6', 'https://localhost:8443/loginCallback', 1, 1),
-(8, 'LULULTEST', 'lulult_fb2cc780-d9a2-492a-aaca-db2996e103fb', '208993db-d2da-4192-ad24-490666b6d551', 'https://localhost:8443', 0, 1);
+(7, 'CubbyHole Website', 'cubbyh_b1175f3f-9c52-4205-adfd-c9a63f7cbecb', 'df8b08e6-215f-49ac-86fa-1212e7c9e0e6', 'https://localhost:8443/loginCallback', 1, 1),
+(9, 'CubbyHole Developer Center', 'cubbyh_28af8ca9-368e-4ce1-8586-e2690ead096f', 'd923c923-efa9-44bd-8980-eb541660b67e', 'https://localhost:8445/loginCallback', 0, 1);
 
 -- --------------------------------------------------------
 
