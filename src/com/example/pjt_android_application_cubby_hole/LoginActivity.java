1d48d1e8d2e8e7e9ebce20e96ea68ab6b5607086
Kupay <145731@supinfo.com>
2014-04-05 07:23:17 +0200

    test d'affichage de page web hebergée localement
    Signed-off-by: Kupay <145731@supinfo.com>

--- src/com/example/pjt_android_application_cubby_hole/LoginActivity.java
+++ src/com/example/pjt_android_application_cubby_hole/LoginActivity.java
@@ -1,22 +1,43 @@
 package com.example.pjt_android_application_cubby_hole;
 
+import com.cubbyhole.library.http.CBHttp;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.webkit.WebView;
 
 public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		
+		//setContentView(R.layout.activity_login);
+		
+//		WebView webview = new WebView(this);
+//		setContentView(webview);
+//		webview.loadUrl("https://10.0.2.2:8444");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
+		return true;	
 	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		
+		Uri uri = Uri.parse("https://10.0.2.2:8444");
+		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+		startActivity(intent);
+	}
+	
+	
 
 }
