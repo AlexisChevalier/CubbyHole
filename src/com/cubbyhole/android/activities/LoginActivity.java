f6bae24bd51edda146403afe17cc4ce5e494b214
Civilian <jeanbaptiste.e@hotmail.fr>
2014-05-28 02:43:12 +0200

    Renamed package + minSdkVersion from 7 to 8
    - 8 cuz of SslError class

--- src/com/cubbyhole/android/activities/LoginActivity.java
+++ src/com/cubbyhole/android/activities/LoginActivity.java
@@ -5,16 +5,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 
+import com.cubbyhole.android.R;
 import com.cubbyhole.android.components.AuthWebView;
 import com.cubbyhole.android.utils.CHC;
 import com.cubbyhole.android.utils.ssl.SSLManager;
-import com.example.pjt_android_application_cubby_hole.R;
 
 public class LoginActivity extends Activity {
 
 	private static final String	TAG	= LoginActivity.class.getName();
-	
-	private AuthWebView mAuthWebView;
+
+	private AuthWebView			mAuthWebView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,7 +25,6 @@ public class LoginActivity extends Activity {
 		if (CHC.IGNORE_NOT_TRUSTED_CERT) {
 			SSLManager.allowNotTrustedCertificates();
 		}
-
 
 		String url = CHC.OAUTH_URL_ACCESS_CODE //
 				+ CHC.PARAM_STARTER //
@@ -42,21 +41,17 @@ public class LoginActivity extends Activity {
 				+ CHC.OAUTH_PARAM_ACCESS_CODE;
 
 		Log.d(TAG, url);
-		try
-		{
+		try {
 			mAuthWebView.loadUrl(url);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		
 	}
 
 	private void bindView() {
-		mAuthWebView = (AuthWebView)findViewById(R.id.authWebView);
-		
+		mAuthWebView = (AuthWebView) findViewById(R.id.authWebView);
+
 	}
 
 	@Override
