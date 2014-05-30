7795527714a9e37d74971086681c04e2fdd47b29
Civilian <jeanbaptiste.e@hotmail.fr>
2014-05-30 04:44:25 +0200

    Android async implementation of the api methods.

---
+++ src/com/cubbyhole/android/api/CubbyHoleClient.java
@@ -0,0 +1,75 @@
+package com.cubbyhole.android.api;
+
+import com.cubbyhole.android.api.AsyncApiRequest.IApiRequestHandler;
+import com.cubbyhole.library.api.CubbyHoleImpl;
+import com.cubbyhole.library.api.entities.CHAccount;
+import com.cubbyhole.library.api.entities.CHFile;
+import com.cubbyhole.library.api.entities.CHFolder;
+
+public class CubbyHoleClient {
+	private static final String		TAG	= CubbyHoleClient.class.getName();
+
+	private static CubbyHoleClient	mInstance;
+	private CubbyHoleImpl			mImpl;
+
+	private CubbyHoleClient() {
+		mImpl = CubbyHoleImpl.getInstance();
+	}
+
+	public static CubbyHoleClient getInstance() {
+		if (mInstance == null) {
+			mInstance = new CubbyHoleClient();
+		}
+		return mInstance;
+	}
+
+	public void Initialize(IApiRequestHandler<Void> handler, String token) {
+		final String method = "Initialize";
+		new AsyncApiRequest<Void>(handler, mImpl, method).execute(token);
+	}
+
+	public void getAccount(IApiRequestHandler<CHAccount> handler) {
+		final String method = "getAccount";
+		new AsyncApiRequest<CHAccount>(handler, mImpl, method).execute();
+	}
+
+	public void updateAccount(IApiRequestHandler<CHAccount> handler, CHAccount account) {
+		final String method = "updateAccount";
+		new AsyncApiRequest<CHAccount>(handler, mImpl, method).execute(account);
+	}
+
+	public void getRootFolder(IApiRequestHandler<CHFolder> handler) {
+		final String method = "getRootFolder";
+		new AsyncApiRequest<CHFolder>(handler, mImpl, method).execute();
+	}
+
+	public void createFolder(IApiRequestHandler<CHAccount> handler, CHFolder parentFolder,
+			String folderName) {
+		final String method = "createFolder";
+		new AsyncApiRequest<CHAccount>(handler, mImpl, method).execute(parentFolder, folderName);
+	}
+
+	public void updateFolder(IApiRequestHandler<CHAccount> handler, CHFolder folder) {
+		final String method = "updateFolder";
+		new AsyncApiRequest<CHAccount>(handler, mImpl, method).execute(folder);
+	}
+
+	public void deleteFolder(IApiRequestHandler<CHAccount> handler, CHFolder folder) {
+		final String method = "deleteFolder";
+		new AsyncApiRequest<CHAccount>(handler, mImpl, method).execute(folder);
+	}
+
+	public void uploadFile(IApiRequestHandler<CHAccount> handler, CHFolder parentFolder, String path) {
+
+	}
+
+	public void downloadFile(IApiRequestHandler<CHAccount> handler, CHFile file) {
+
+	}
+
+	public void deleteFile(IApiRequestHandler<Boolean> handler, CHFile file) {
+		final String method = "deleteFile";
+		new AsyncApiRequest<Boolean>(handler, mImpl, method).execute(file);
+	}
+
+}
