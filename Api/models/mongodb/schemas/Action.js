3edf1d2695ba752c944d8ea94ae16eb9614ea1ac
Alexis Chevalier <alexis.chevalier1@gmail.com>
2014-06-14 01:56:15 +0200

    Updated schema

--- Api/models/mongodb/schemas/Action.js
+++ Api/models/mongodb/schemas/Action.js
@@ -5,7 +5,7 @@ module.exports = new Schema({
     "relatedId": Schema.Types.ObjectId,
     "actorUserId": Number,
     "concernedUsers": [Number],
-    "action": String, //create/edit/update/move/copy/delete/shareUpdated/shareAdded/shareRemoved/download
+    "action": String, //create/edit/update/move/copy/delete/shareUpdated/shareAdded/shareRemoved/download/publicDownload
     "type": String, //file or folder
     "time": Date,
     "length": Number
