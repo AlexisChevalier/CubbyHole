4c3131be0111154254066ed694574c9f35a53ffe
Civilian <jeanbaptiste.e@hotmail.fr>
2014-06-14 19:38:58 +0200

    Added fileExists, folderExists and createFolder in SystemHelper

--- src/com/cubbyhole/library/system/SystemHelper.java
+++ src/com/cubbyhole/library/system/SystemHelper.java
@@ -1,5 +1,7 @@
 package com.cubbyhole.library.system;
 
+import java.io.File;
+
 /**
  * Class that provides static methods helper related to the system.
  */
@@ -21,5 +23,34 @@ public class SystemHelper {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	/**
+	 * Check if the file at the given path exists.
+	 * @param path - the path of the file to check (include the filename and its extension)
+	 * @return <code>true</code> if it exists, <code>false</code> otherwise.
+	 */
+	public static boolean fileExists(String path) {
+		File f = new File(path);
+		return f.exists() && !f.isDirectory();
+	}
+
+	/**
+	 * Check if the folder at the given path exists.
+	 * @param path - the path of the file to check.
+	 * @return <code>true</code> if it exists, <code>false</code> otherwise.
+	 */
+	public static boolean folderExists(String path) {
+		File f = new File(path);
+		return f.exists() && f.isDirectory();
+	}
+
+	/**
+	 * Create folder (and all parent folder) with the given path.
+	 * @param path - the path that represents the full path of the new folder.
+	 * @return <code>true</code> if success, <code>false</code> otherwise.
+	 */
+	public static boolean createFolder(String path) {
+		return (new File(path)).mkdirs();
 	}
 }
